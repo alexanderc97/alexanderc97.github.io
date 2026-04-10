ba314fb2bd761805b05abd55731d1bee94741169
Alex <alexcanning048@gmail.com>
2026-04-10 17:41:08 -0400

    Added Classes
    Added Shape base class and refractor Ball to extend Shape

--- part4-evil-circle/main.js
+++ part4-evil-circle/main.js
@@ -4,6 +4,10 @@ const ctx = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+// reference to ball counter paragraph
+const para = document.querySelector('p');
+let count = 0;
+
 // function to generate random number
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,13 +18,21 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-// Ball class
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+// Shape base class - parent of Ball and EvilCircle
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    this.exists = true;
+  }
+}
+
+// Ball class - inherits from Shape
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
   }
@@ -45,18 +57,16 @@ class Ball {
     if ((this.y - this.size) <= 0) {
       this.velY = -(this.velY);
     }
-
     this.x += this.velX;
     this.y += this.velY;
   }
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (this !== ball && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
@@ -64,36 +74,3 @@ class Ball {
     }
   }
 }
-
-// Create 25 balls and store them in an array
-const balls = [];
-
-while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size
-  );
-  balls.push(ball);
-}
-
-// Animation loop
-function loop() {
-  // Semi-transparent background creates motion trail effect
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-  ctx.fillRect(0, 0, width, height);
-
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
-
-  requestAnimationFrame(loop);
-}
-
-loop();
